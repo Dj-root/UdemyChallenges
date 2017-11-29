efd68610f0f282ab55f1d7516d51ce0180cd2da7
holms <holms.html@gmail.com>
2017-11-30 00:39:50 +0200

    Cleated MusicDB class

---
+++ src/com/holms/unit22/MusicDB.java
@@ -0,0 +1,15 @@
+package com.holms.unit22;
+
+import com.holms.unit22.model.Datasource;
+
+public class MusicDB {
+    public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        if (!datasource.open()){
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        datasource.close();
+    }
+}
