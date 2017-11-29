54d79cbd096325cc9e81205a881dfa2c3ea36706
holms <holms.html@gmail.com>
2017-11-30 01:27:31 +0200

    Wrote select statement and tested

--- src/com/holms/unit22/MusicDB.java
+++ src/com/holms/unit22/MusicDB.java
@@ -1,6 +1,9 @@
 package com.holms.unit22;
 
+import com.holms.unit22.model.Artist;
 import com.holms.unit22.model.Datasource;
+
+import java.util.List;
 
 public class MusicDB {
     public static void main(String[] args) {
@@ -10,7 +13,18 @@ public class MusicDB {
             return;
         }
 
+        List<Artist> artists = datasource.queryArtists();
+        if (artists==null){
+            System.out.println("No artists!");
+            return;
+        }
+
+        for (Artist artist:artists){
+            System.out.println("ID = "+artist.getId()+" , Name = "+artist.getName());
+        }
+
         datasource.close();
+
         System.out.println("It's finished");
     }
 }
--- src/com/holms/unit22/model/Datasource.java
+++ src/com/holms/unit22/model/Datasource.java
@@ -1,8 +1,8 @@
 package com.holms.unit22.model;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Datasource {
     public static final String DB_NAME = "music.db";
@@ -46,4 +46,49 @@ public class Datasource {
         }
     }
 
+
+    public List<Artist> queryArtists() {
+
+//        Statement statement = null;
+//        ResultSet results = null;
+
+        try (Statement statement = conn.createStatement();
+             ResultSet results = statement.executeQuery("SELECT * FROM " + TABLE_ATRISTS)) {
+//            statement = conn.createStatement();
+//            results = statement.executeQuery("SELECT * FROM " + TABLE_ATRISTS);
+
+            List<Artist> artists = new ArrayList<>();
+            while (results.next()) {
+                Artist artist = new Artist();
+                artist.setId(results.getInt(COLUMN_ARTIST_ID));
+                artist.setName(results.getString(COLUMN_ARTIST_NAME));
+                artists.add(artist);
+            }
+
+            return artists;
+
+        } catch (SQLException e) {
+            System.out.println("Query failed " + e.getMessage());
+            return null;
+        }
+//        } finally {
+//            try {
+//                if (results != null) {
+//                    results.close();
+//                }
+//            } catch (SQLException e) {
+//                System.out.println("Error closing result set " + e.getMessage());
+//            }
+//
+//            try {
+//                if (statement != null) {
+//                    statement.close();
+//                }
+//            } catch (SQLException e) {
+//                System.out.println("Error closing Statement " + e.getMessage());
+//            }
+//
+//        }
+
+    }
 }
